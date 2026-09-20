4c16eea8c963778d5bd1ff92223d3792bffe1d66
Tomaselbert2000 <tomas@debian.tomas>
2026-09-20 19:37:49 -0300

    feat: completa implementación inicial de capa Service y agrega excepción de dominio

---
+++ src/main/java/com/todocodeacademy/prueba_tecnica_ferreteria/exceptions/BusinessException.java
@@ -0,0 +1,9 @@
+package com.todocodeacademy.prueba_tecnica_ferreteria.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+
+        super(message);
+    }
+}
