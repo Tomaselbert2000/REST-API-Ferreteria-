e04eae6c132530dee2cd7bc0890369f5193bc0c5
Tomaselbert2000 <tomas@debian.tomas>
2026-09-20 16:28:56 -0300

    feat: agrega capa DTO al proyecto, mejoras menores en modelo de datos

--- src/main/java/com/todocodeacademy/prueba_tecnica_ferreteria/dto/ProductInputDTO.java
+++ src/main/java/com/todocodeacademy/prueba_tecnica_ferreteria/dto/ProductInputDTO.java
@@ -1,7 +1,7 @@
-package com.todocodeacademy.prueba_tecnica_ferreteria.model;
+package com.todocodeacademy.prueba_tecnica_ferreteria.dto;
 
 import com.todocodeacademy.prueba_tecnica_ferreteria.enums.ProductCategory;
-import jakarta.persistence.*;
+import com.todocodeacademy.prueba_tecnica_ferreteria.model.Brand;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,24 +11,11 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
-@Table(name = "products")
-public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public final class ProductInputDTO {
 
     private String name;
-
-    @ManyToOne
-    @JoinColumn(name = "brand_id")
     private Brand brand;
-
-    @Enumerated(EnumType.STRING)
     private ProductCategory category;
-
     private BigDecimal price;
-
     private Integer currentStock;
 }
