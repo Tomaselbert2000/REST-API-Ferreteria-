f72d3df62e56984a7c7d7cc74b6ef8193dfc4cad
Tomaselbert2000 <tomas@debian.tomas>
2026-09-20 01:09:57 -0300

    feat: creación de rama develop

---
+++ src/main/java/com/todocodeacademy/prueba_tecnica_ferreteria/PruebaTecnicaFerreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.prueba_tecnica_ferreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaFerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaFerreteriaApplication.class, args);
+	}
+
+}
